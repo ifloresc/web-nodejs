d9cf0310633b173782c659ed3a60c51f07bacf5b
Ivan Flores <ivan.flores@globant.com>
2017-03-28 17:11:40 -0300

    Se agrega gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,44 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-var pug = require('gulp-pug');
-var uglify = require('gulp-uglify');
-var plumber = require('gulp-plumber');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp'),
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload,
+	$ = require('gulp-load-plugins')();
 
 gulp.task('sass', function() {
 	return gulp.src('src/public/css/*.sass') // Leer un archivo
-  		.pipe(plumber())	
-    	.pipe(sourcemaps.init())
-		.pipe(sass()) // Compilar SASS
-        .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false})) // Autoprfixer
+  		.pipe($.plumber())	
+    	.pipe($.sourcemaps.init())
+		.pipe($.sass()) // Compilar SASS
+        .pipe($.autoprefixer({ browsers: ['last 2 versions'], cascade: false})) // Autoprfixer
 		.pipe(gulp.dest('.tmp')) // Guardar archivo
-        .pipe(sourcemaps.write())
+        .pipe($.sourcemaps.write())
 		.pipe(reload({ stream: true })); // Enviar cambios al navegador
 });
 
 gulp.task('pug', function() {
 	return gulp.src('src/public/*.pug')
-  		.pipe(plumber())	
-	  .pipe(pug())
+  		.pipe($.plumber())	
+	  .pipe($.pug())
 	  .pipe(gulp.dest('.tmp'))	
 });
 
 gulp.task('sass:prod', function() {
 	return gulp.src('src/public/css/*.sass') // Leer un archivo
-  		.pipe(plumber())	
-		.pipe(sass({ outputStyle: 'compressed'})) // Compilar SASS
-        .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false})) // Autoprfixer
+  		.pipe($.plumber())	
+		.pipe($.sass({ outputStyle: 'compressed'})) // Compilar SASS
+        .pipe($.autoprefixer({ browsers: ['last 2 versions'], cascade: false})) // Autoprfixer
 		.pipe(gulp.dest('dist')); // Enviar cambios al navegador
 });
 
 gulp.task('pug:prod', function() {
 	return gulp.src('src/public/*.pug')
-  		.pipe(plumber())	
-	  .pipe(pug())
+  		.pipe($.plumber())	
+	  .pipe($.pug())
 	  .pipe(gulp.dest('dist'))	
 });
 gulp.task('js', function() {
 	gulp.src('./src/**/*.js')
-  		.pipe(plumber())	
-		.pipe(uglify({compress:true}))
+  		.pipe($.plumber())	
+		.pipe($.uglify({compress:true}))
 		.pipe(gulp.dest('dist'))
 });
 
@@ -64,4 +59,22 @@ gulp.task('server', ['sass', 'pug'], function() {
 
 gulp.task('build', ['sass:prod', 'pug:prod', 'js']);
 
+gulp.task('upload', ['build'], function() {
+	var ftp = ftpConnection();
+	var remoteFolder = '/_demos/acamica/demo-1-2/';
+	return gulp.src('dist/**', { base: 'dist', buffer: false})
+		.pipe($.ftp.newer(remoteFolder))
+		.pipe($.ftp.dest(remoteFolder));
+});
+
 gulp.task('default', ['sass']);
+
+function ftpConnection() {
+	return $.ftp.create({
+		host: 'acamica.com',
+		user: 'sergio',
+		password: process.env.FTP_PWD,
+		parallel: 5,
+		log: gutil.log
+	})
+};
